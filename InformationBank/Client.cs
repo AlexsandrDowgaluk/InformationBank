260cb4a9dfdb3272db922081997bfa43dab43676
AlexsandrDowgaluk <Dowgaluk88@gmail.com>
2019-09-18 21:32:34 +0300

    Добавьте файлы проекта.

---
+++ InformationBank/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationBank
+{
+    class Client
+    {
+
+        private long number;
+        private string type;
+        private double balance;
+
+        public Client()
+        {
+         number = 5656454648465654;
+         type = "";
+         balance=23.2;
+
+
+    }
+
+
+        public long Number {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+
+        }
+        public string Type {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+
+        }
+        public double Balance {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+
+        }
+    }
+}
---
+++ InformationBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client Alex = new Client();
+            Alex.Number = 165654616445461;
+            Alex.Type = " Расчетный счет ";
+            Alex.Balance = 1516.156;
+            Console.WriteLine(Alex.Number +"\n"+ Alex.Type + "\n"+Alex.Balance);
+            Console.ReadKey();
+
+        }
+    }
+}
